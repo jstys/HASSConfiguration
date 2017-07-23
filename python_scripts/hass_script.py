a72de51f39d9e921a547e196b7fc703e9405948d
jstys <jstys92@gmail.com>
2017-07-23 12:47:23 -0400

    Made a more generic python script to call any hASS internal script

--- python_scripts/hass_script.py
+++ python_scripts/hass_script.py
@@ -1,7 +1,7 @@
 #!/srv/homeassistant/bin/python3
-
+import sys
 import homeassistant.remote as remote
 
 api = remote.API('127.0.0.1')
 
-remote.call_service(api, "script", "goal_horn")
+remote.call_service(api, "script", sys.argv[1])
