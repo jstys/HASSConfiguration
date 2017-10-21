7011c95eeeb77b9cf6ceb9acf14f0c3cab59d1a4
jstys <jstys92@gmail.com>
2017-10-21 13:04:36 -0400

    Add support for multi-room snips.  Fix issue with passing JSON to shell_command.  Add a group for lamps.

--- python_scripts/snips_intent_processor.py
+++ python_scripts/snips_intent_processor.py
@@ -4,17 +4,44 @@ import sys
 import homeassistant.remote as remote
 
 api = remote.API('127.0.0.1')
+room = None
+
+def handle_json_request(json_message):
+    intent = json_message['intent']['intentName']
+    if intent == "ActivateObject":
+        handle_activate_deactivate_object(json_message, True)
+    elif intent == "DeactivateObject":
+        handle_activate_deactivate_object(json_message, False)
+    elif intent == "ActivateLightColor":
+        tts_say("Sorry, I can't do that yet")
+    else:
+        tts_say("Sorry, I don't understand what you're asking for")
+
+def handle_activate_deactivate_object(json_message, activate):
+    devices = [device for device in json_message.get('slots') if device['entity'] == "objectType"]
+    for device in devices:
+        action = "on" if activate else "off"
+        tts_say("Turning {} {}".format(action, device['value']['value']))
+
 
 def tts_say(message):
-    remote.call_service(api, "script", "snips_voice", {"message": message})
+    remote.call_service(api, "script", "snips_voice", {"message": message, "room": room})
 
 if __name__ == '__main__':
-    raw_payload = sys.argv[1]
+    topic = sys.argv[1]
+    raw_payload = sys.argv[2]
+
+    with open('/tmp/snips_input.txt', 'w') as debug_file:
+        debug_file.write(raw_payload)
+
     json_payload = {}
     try:
         json_payload = json.loads(raw_payload)
     except:
-        tts_say("Unable to parse intent")
+        tts_say("Unable to parse request")
         sys.exit(1)
 
-    tts_say("Successfully parsed intent")
+    room = topic.split("/")[1]
+
+    tts_say("Received command from {} snips".format(room.replace("_", " ")))
+    handle_json_request(json_payload)
