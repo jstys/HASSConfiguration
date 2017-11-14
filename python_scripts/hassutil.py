5cb3fcace3513fea611155ce013a43379f966dd8
jstys <jstys92@gmail.com>
2017-11-13 19:05:49 -0500

    Fix an issue with working directory in the google_assistant.service.  Fix a minor bug in hassutil.  Update tts in intent_processor to remove contractions.

--- python_scripts/hassutil.py
+++ python_scripts/hassutil.py
@@ -7,8 +7,8 @@ GROUPS = "groups.yaml"
 class Entity(object):
     def __init__(self, fully_qualified_name):
         fqn_split = fully_qualified_name.split(".")
-        self.domain = fqn_split[0]
-        self.name = fqn_split[1]
+        self.domain = fqn_split[0].strip()
+        self.name = fqn_split[1].strip()
         self.entity_id = fully_qualified_name
 
 def read_config_file(filename):
@@ -53,9 +53,9 @@ def get_group_switches_and_lights(group):
     if group_entity is not None:
         for entity_name in group_entity['entities']:
             entity = Entity(entity_name)
-            if entity.domain == "switch" or entity.domain == "light":
+            if entity.domain in ["light", "switch"]:
                 result.append(entity)
             elif entity.domain == "group":
-                result.extend(get_group_switches_and_lights(entity_name))
+                result.extend(get_group_switches_and_lights(entity.name))
 
     return result
--- python_scripts/intent_processor.py
+++ python_scripts/intent_processor.py
@@ -3,7 +3,6 @@ import json
 import sys
 import random
 import hassutil
-from hassutil import Entity
 import homeassistant.remote as remote
 
 BROADCAST_ROOM = "broadcast"
@@ -17,17 +16,17 @@ def handle_json_request(json_message):
     if intent == "PowerIntent":
         handle_power_intent(json_message)
     elif intent == "MediaIntent":
-        tts_say("Sorry, I can't control media yet")
+        tts_say("Sorry, I cant control media yet")
     elif intent == "LevelIntent":
-        tts_say("Sorry, I can't control device levels yet")
+        tts_say("Sorry, I cant control device levels yet")
     elif intent == "BroadcastIntent" or intent == "TalkIntent":
-        tts_say("Sorry, I can't broadcast messages yet")
+        tts_say("Sorry, I cant broadcast messages yet")
     elif intent == "ListIntent":
-        tts_say("Sorry, I can't manage your lists yet")
+        tts_say("Sorry, I cant manage your lists yet")
     elif intent == "SceneIntent":
-        tts_say("Actually, it's fuckin not")
+        tts_say("Actually, its fuckin not")
     else:
-        tts_say("Sorry, I don't understand what you're asking")
+        tts_say("Sorry, I dont understand what youre asking")
 
 def handle_power_intent(json_message):
     room = json_message.get('Room')
@@ -42,7 +41,11 @@ def handle_power_intent(json_message):
             device = json_message[objectType].lower().replace(" ", "_")
             deviceType = objectType
         except KeyError:
+            device = None
+            deviceType = None
             continue
+        else:
+            break
 
     if deviceType == "LightObject":
         device = "light"
@@ -59,7 +62,6 @@ def handle_power_intent(json_message):
 
     level = json_message.get('Percentage')
     allMod = json_message.get('AllModifier') is not None
-
 
     if allMod:
         if deviceType == "LightObject":
@@ -102,7 +104,7 @@ if __name__ == '__main__':
         json_payload = json.loads(raw_payload)
         received_room = topic.split("/")[1]
     except:
-        tts_say("Sorry, I'm unable to understand your request")
+        tts_say("Sorry, Im unable to understand your request")
         sys.exit(1)
 
     handle_json_request(json_payload)
