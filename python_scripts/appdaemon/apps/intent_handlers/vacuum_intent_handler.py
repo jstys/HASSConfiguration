55abc898763c8d6a59f4d4938c85c88b1ea84302
Jim Stys <jstys92@gmail.com>
2018-01-22 19:25:28 -0500

    Minor fixes for the vacuum intent handler

--- python_scripts/appdaemon/apps/intent_handlers/vacuum_intent_handler.py
+++ python_scripts/appdaemon/apps/intent_handlers/vacuum_intent_handler.py
@@ -6,13 +6,13 @@ _DEFAULT_VACUUM = "xiaomi_vacuum_cleaner"
 
 def handle(api, json_message):
     action = json_message.get("MediaVerb")
-    vacuum = json_message.get("VacuumName", default=_DEFAULT_VACUUM)
+    vacuum = ".".join(["vacuum", json_message.get("VacuumName", _DEFAULT_VACUUM)])
     if action == "start":
         api.call_service("/".join(["vacuum", "turn_on"]), entity_id=vacuum)
     elif action == "pause" or action == "resume":
         api.call_service("/".join(["vacuum", "start_pause"]), entity_id=vacuum)
     elif action == "stop":
-        api.call_service("/".join(["vacuum", "stop"]), entity_id=vacuum)
+        api.call_service("/".join(["vacuum", "return_to_base"]), entity_id=vacuum)
     else:
         # TODO: indicate error
         pass
