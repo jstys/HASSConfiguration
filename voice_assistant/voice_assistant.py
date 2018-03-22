c1eefa225dccc11e84af29b0e050bb91d5f65be4
Jim Stys <jstys92@gmail.com>
2018-03-21 21:27:17 -0400

    Fix additional bug when parsing yaml config.

--- voice_assistant/voice_assistant.py
+++ voice_assistant/voice_assistant.py
@@ -74,14 +74,14 @@ class VoiceAssistant():
             
         try:
             self._assistant_type = self._common_config['assistant_type']
-            self._assistant_config = self._configuration[self._assistant_type]
+            self._assistant_config = self._configuration['assistant'][self._assistant_type]
         except:
             print("Invalid configuration of 'assistant_type' in voice_assistant.yaml")
             return False
             
         try:
             self.connection_protocol = self._common_config['connection_protocol']
-            self.connection_config = self._configuration[self._connection_protocol]
+            self.connection_config = self._configuration['connection'][self._connection_protocol]
         except:
             print("Invalid configuration of 'connection_protocol' in voice_assistant.yaml")
             return False
