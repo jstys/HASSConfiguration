6b00cf0393dc31c43c08a60dd8cecb961e8a2faa
Jim Stys <jstys92@gmail.com>
2018-03-21 23:06:29 -0400

    Fix missing positional argument when constructing mqtt_client_connection.

--- voice_assistant/connections/mqtt_client_connection.py
+++ voice_assistant/connections/mqtt_client_connection.py
@@ -11,7 +11,7 @@ class MqttClientConnection(IConnection):
     MQTT_V_3_1_1 = "3.1.1"
     
     def __init__(self, config, assistant_room, on_tts, on_broadcast, on_ask, on_broadcast_ask):
-        super().__init__(config, on_tts, on_broadcast, on_ask, on_broadcast_ask)
+        super().__init__(config, assistant_room, on_tts, on_broadcast, on_ask, on_broadcast_ask)
         self._mqtt_client = None
         self._assistant_room = None
         self._client_id = None
