e0ee1cd4fc04887b4dd3a6003d78caf855ce8037
Jim Stys <jstys92@gmail.com>
2018-03-22 17:49:13 -0400

    Fix an issue where the assistant_room field wasn't being set in the iconnection class.

--- voice_assistant/connections/iconnection.py
+++ voice_assistant/connections/iconnection.py
@@ -1,6 +1,7 @@
 class IConnection():
     def __init__(self, config, assistant_room, on_tts, on_broadcast, on_ask, on_broadcast_ask):
         self._config = config
+        self._assistant_room = assistant_room
         self._on_tts_message = on_tts
         self._on_broadcast_message = on_broadcast
         self._on_ask_message = on_ask
