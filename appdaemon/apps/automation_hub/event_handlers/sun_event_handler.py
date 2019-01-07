db23da79e782692c7bf0726890643ffba46b9872
Jim Stys <jstys92@gmail.com>
2019-01-07 17:16:04 +0000

    Auto turn on front garden lights at sunset and turn off at sunrise

--- appdaemon/apps/automation_hub/event_handlers/sun_event_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/sun_event_handler.py
@@ -3,6 +3,7 @@ from automation_hub import state_machine
 from util import logger
 from events.sunrise_event import SunriseEvent
 from events.sunset_event import SunsetEvent
+from actions.LightAction import LightAction
 
 def register_callbacks():
     event_dispatcher.register_callback(on_sunrise, SunriseEvent.__name__)
@@ -11,6 +12,10 @@ def register_callbacks():
 def on_sunrise(event):
     state_machine.set_state(state_machine.SUN_UP_STATE, True)
     state_machine.set_state(state_machine.SLEEP_STATE, False)
+    
+    LightAction().add_light("front_garden_lights").turn_off()
 
 def on_sunset(event):
     state_machine.set_state(state_machine.SUN_UP_STATE, False)
+    
+    LightAction().add_light("front_garden_lights").turn_on()
