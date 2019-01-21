b270438a9496ab986578c279283698a6f700b3b6
Jim Stys <jstys92@gmail.com>
2019-01-21 17:19:49 +0000

    Add initial support for MediaPlayerAction.  Add logic to handle slots in snips intents

---
+++ appdaemon/apps/automation_hub/actions/media_player_action.py
@@ -0,0 +1,49 @@
+from util.entity_map import name_map
+from util import hassutil
+from util import logger
+
+class MediaPlayerAction():
+    def __init__(self):
+        self._media_players = []
+
+    def add_media_players(self, media_players):
+        for media_player in media_players:
+            self.add_media_player(media_player)
+
+        return self
+
+    def add_media_player(self, media_player):
+        if media_player in name_map:
+            self._media_players.append(hassutil.Entity(name_map[media_player]))
+        else:
+            logger.error("Unable to add unknown light to MediaPlayerAction: {}".format(media_player))
+
+        return self
+
+    def turn_on(self):
+        for media_player in self._media_players:
+            hassutil.turn_on(media_player)
+
+    def turn_off(self):
+        for media_player in self._media_players:
+            hassutil.turn_off(media_player)
+            
+    def pause(self):
+        for media_player in self._media_players:
+            hassutil.call_service("media_player", "pause", entity_id=media_player)
+        
+    def play(self):
+        for media_player in self._media_players:
+            hassutil.call_service("media_player", "play", entity_id=media_player)
+            
+    def change_input(self, input_name):
+        for media_player in self._media_players:
+            hassutil.call_service("media_player", "select_source", entity_id=media_player, source=input_name)
+            
+    def mute(self):
+        for media_player in self._media_players:
+            hassutil.call_service("media_player", "volume_mute", entity_id=media_player, is_volume_muted=True)
+            
+    def unmute(self):
+        for media_player in self._media_players:
+            hassutil.call_service("media_player", "volume_mute", entity_id=media_player, is_volume_muted=False)
--- appdaemon/apps/automation_hub/event_handlers/snips_intent_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/snips_intent_handler.py
@@ -6,6 +6,7 @@ from util import logger
 from util.entity_map import room_map
 from util.entity_map import find_room_entities
 from actions.light_action import LightAction
+from actions.media_player_action import MediaPlayerAction
 from events.mqtt_event import MQTTEvent
 
 def event_filter(event):
@@ -24,42 +25,82 @@ def on_intent(event):
     source = parsed.get("siteId")
     raw = parsed.get("input")
     intent = parsed.get("intent")
+    slotMap = {}
+    slots = parsed.get("slots")
+    if slots:
+        for slot in slots:
+            slotMap[slot.get("slotName")] = slot.get("value")
     if intent and source and raw:
         logger.info("Received Snips intent from {} with raw input ({})".format(source, raw))
-        name = intent.get("intentName")
-        if name == "jstys:thermostatSet":
-            handle_thermostat(intent, source, raw)
-        elif name == "jstys:tvOn":
-            handle_tv_on(intent, source, raw)
-        elif name == "jstys:tvOff":
-            handle_tv_off(intent, source, raw)
-        elif name == "jstys:lightOn":
-            handle_light_on(intent, source, raw)
-        elif name == "jstys:lightOff":
-            handle_light_off(intent, source, raw)
+        name = intent.get("intentName").split(":")
+        name = name[len(name)-1]
+        if name == "thermostatSet":
+            handle_thermostat(intent, source, raw, slotMap)
+        elif name == "thermostatShift":
+            pass
+        elif name == "thermostatTurnOff":
+            pass
+        elif name == "tvOn":
+            handle_tv_on(intent, source, raw, slotMap)
+        elif name == "tvOff":
+            handle_tv_off(intent, source, raw, slotMap)
+        elif name == "tvInput":
+            handle_tv_input(intent, source, raw, slotMap)
+        elif name == "lightOn":
+            handle_light_on(intent, source, raw, slotMap)
+        elif name == "lightOff":
+            handle_light_off(intent, source, raw, slotMap)
+        elif name == "SetTimer":
+            pass
+        elif name == "StopTimer":
+            pass
+        elif name == "VacuumStart":
+            pass
+        elif name == "VacuumStop":
+            pass
     else:
         logger.error("Missing valid snips intent")
 
-def handle_thermostat(intent, source, raw):
+def handle_thermostat(intent, source, raw, slotMap):
     logger.info("Received Thermostat set intent from snips")
 
-def handle_tv_on(intent, source, raw):
+def handle_tv_on(intent, source, raw, slotMap):
     logger.info("Received TV On intent from snips")
+    
+    room = slotMap["room"]["value"] if "room" in slotMap else source
 
-def handle_tv_off(intent, source, raw):
+    if room in room_map:
+        media_players = find_room_entities("tv", room).extend(find_room_entities("speakers", room))
+        MediaPlayerAction().add_media_players(media_players).turn_on()
+        
+
+def handle_tv_off(intent, source, raw, slotMap):
     logger.info("Received TV Off intent from snips")
+    
+    room = slotMap["room"]["value"] if "room" in slotMap else source
 
-def handle_light_on(intent, source, raw):
+    if room in room_map:
+        media_players = find_room_entities("tv", room).extend(find_room_entities("speakers", room))
+        MediaPlayerAction().add_media_players(media_players).turn_off()
+        
+def handle_tv_input(intent, source, raw, slotMap):
+    logger.info("Received TV Input intent from snips")
+
+def handle_light_on(intent, source, raw, slotMap):
     logger.info("Received Light On intent from snips")
+    
+    room = slotMap["room"]["value"] if "room" in slotMap else source
 
-    if source in room_map:
-        lights = find_room_entities("light", source)
+    if room in room_map:
+        lights = find_room_entities("light", room)
         LightAction().add_lights(lights).turn_on()
 
-def handle_light_off(intent, source, raw):
+def handle_light_off(intent, source, raw, slotMap):
     logger.info("Received Light off intent from snips")
 
-    if source in room_map:
-        lights = find_room_entities("light", source)
+    room = slotMap["room"]["value"] if "room" in slotMap else source
+
+    if room in room_map:
+        lights = find_room_entities("light", room)
         LightAction().add_lights(lights).turn_off()
     
