1fa0d33868e35a7aef93d5714d38c97f6c24c428
Jim Stys <jstys92@gmail.com>
2019-01-22 17:43:53 +0000

    Add nobody/somebody home handling to presence_handler

--- appdaemon/apps/automation_hub/event_handlers/presence_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/presence_handler.py
@@ -1,8 +1,13 @@
 from automation_hub import event_dispatcher
 from automation_hub import state_machine
 from util import logger
+from util import hassutil
+from util import entity_map
 from events.presence_event import PresenceEvent
 from actions.push_notify_action import PushNotifyAction
+from actions.light_action import LightAction
+from actions.media_player_action import MediaPlayerAction
+from actions.door_lock_action import DoorLockAction
 
 def register_callbacks():
     event_dispatcher.register_callback(on_presence_changed, PresenceEvent.__name__)
@@ -16,6 +21,9 @@ def on_presence_changed(event):
 def on_person_away(name):
     logger.info("{} has left home".format(name))
     
+    if not hassutil.is_someone_home():
+        handle_nobody_home()
+    
     if name == "jim_presence":
         PushNotifyAction().add_target("jim_cell_notify").set_message("Come back soon!").notify()
 
@@ -23,6 +31,28 @@ def on_person_away(name):
 def on_person_home(name):
     logger.info("{} has arrived home".format(name))
     
+    if state_machine.get_state(state_machine.NOBODY_HOME_STATE):
+        handle_somebody_home()
+    
     if name == "jim_presence":
         PushNotifyAction().add_target("jim_cell_notify").set_message("Welcome home!").notify()
         
+        
+def handle_nobody_home():
+    logger.info("Nobody home...")
+    state_machine.set_state(state_machine.NOBODY_HOME_STATE, True)
+    
+    all_lights = entity_map.find_type_entities("light")
+    all_tvs = entity_map.find_type_entities("tv")
+    
+    LightAction().add_lights(all_lights).turn_off()
+    MediaPlayerAction().add_media_players(all_tvs).turn_off()
+    DoorLockAction().add_lock("front_door_lock").lock()
+    #TODO: schedule timer for simulating someone being home
+    
+def handle_somebody_home():
+    logger.info("Someone is home now...")
+    state_machine.set_state(state_machine.NOBODY_HOME_STATE, False)
+    
+    #TODO: cancel timer for simulating someone being home
+    #TODO: set lights / thermostats / tvs
