451212988411b6f3efc2b6947ee46122789f418f
Jim Stys <jstys92@gmail.com>
2019-02-18 17:26:23 +0000

    Add logic to manually trigger motion off for xiaomi sensors

--- appdaemon/apps/automation_hub/event_handlers/hallway_motion_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/hallway_motion_handler.py
@@ -1,5 +1,6 @@
 from automation_hub import event_dispatcher
 from automation_hub import state_machine
+from automation_hub import timer_manager
 from util import logger
 from events.motion_triggered_event import MotionTriggeredEvent
 from events.motion_cleared_event import MotionClearedEvent
@@ -10,17 +11,19 @@ def event_filter(event):
 
 def register_callbacks():
     event_dispatcher.register_callback(on_motion_triggered, MotionTriggeredEvent.__name__, event_filter=event_filter)
-    event_dispatcher.register_callback(on_motion_cleared, MotionClearedEvent.__name__, event_filter=event_filter)
     
 def on_motion_triggered(event):
     logger.info("Hallway motion detected")
+    
+    timer_manager.cancel_timer("hallway_motion_timer")
+    timer_manager.start_timer("hallway_motion_timer", on_motion_cleared, minutes=1)
     
     if not state_machine.get_state(state_machine.SLEEP_STATE):
         LightAction().add_light("hallway_lights").turn_on()
     else:
         LightAction().add_lights(["gateway_light", "first_floor_staircase_led"]).turn_on()
 
-def on_motion_cleared(event):
+def on_motion_cleared():
     logger.info("Hallway motion cleared")
     
     LightAction().add_lights(["hallway_lights", "gateway_light", "first_floor_staircase_led"]).turn_off()
--- appdaemon/apps/automation_hub/event_handlers/landing_motion_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/landing_motion_handler.py
@@ -1,5 +1,6 @@
 from automation_hub import event_dispatcher
 from automation_hub import state_machine
+from automation_hub import timer_manager
 from util import logger
 from events.motion_triggered_event import MotionTriggeredEvent
 from events.motion_cleared_event import MotionClearedEvent
@@ -10,14 +11,16 @@ def event_filter(event):
 
 def register_callbacks():
     event_dispatcher.register_callback(on_motion_triggered, MotionTriggeredEvent.__name__, event_filter=event_filter)
-    event_dispatcher.register_callback(on_motion_cleared, MotionClearedEvent.__name__, event_filter=event_filter)
     
 def on_motion_triggered(event):
     logger.info("Landing motion detected")
     
+    timer_manager.cancel_timer("landing_motion_timer")
+    timer_manager.start_timer("landing_motion_timer", on_motion_cleared, minutes=1)
+    
     LightAction().add_light("landing_light").turn_on()
 
-def on_motion_cleared(event):
+def on_motion_cleared():
     logger.info("Landing motion cleared")
     
     LightAction().add_light("landing_light").turn_off()
