8a768d0ad47942ba9ee79b785ff64875e21f9c0b
Jim Stys <jstys92@gmail.com>
2019-02-18 17:09:24 +0000

    Add an event_handler to sync the kitchen lights off of one switch

---
+++ appdaemon/apps/automation_hub/event_handlers/kitchen_lights_sync_handler.py
@@ -0,0 +1,29 @@
+from automation_hub import event_dispatcher
+from automation_hub import state_machine
+from util import logger
+from events.mqtt_event import MQTTEvent
+from actions.light_action import LightAction
+
+def event_filter(event):
+    return "smartthings/kitchen_fixture/switch" in event.topic
+
+def register_callbacks():
+    event_dispatcher.register_callback(on_message, MQTTEvent.__name__, event_filter=event_filter)
+    
+def on_message(event):
+    topic = event.topic
+    payload = event.payload
+    
+    logger.info("Received Kitchen Fixture MQTT message with topic: {} and payload: {}".format(topic, payload))
+
+    if payload == "on":
+        handle_turned_on()
+    elif payload == "off":
+        handle_turned_off()
+
+def handle_turned_on():
+    LightAction().add_light("kitchen_lights").turn_on()
+    
+def handle_turned_off():
+    LightAction().add_light("kitchen_lights").turn_off()
+    
