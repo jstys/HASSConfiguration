b07e8c8be96590f14a1a08e5b2a9b8dcbdda3b2a
Jim Stys <jstys92@gmail.com>
2019-06-11 12:14:42 +0000

    Don't autostart vacuum in sleep mode

--- appdaemon/apps/automation_hub/event_handlers/sleep_state_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/sleep_state_handler.py
@@ -33,7 +33,6 @@ def on_sleep_state_enabled(event):
     
     DoorLockAction().add_lock("front_entrance_lock").lock()
     LightAction().add_light("manual_off_lights").turn_off()
-    VacuumAction().add_vacuum("robot_vacuum").start()
     assistant_action = AssistantAction().add_assistant("master_bedroom")
     assistant_action.disable_hotword()
     assistant_action.disable_led()
