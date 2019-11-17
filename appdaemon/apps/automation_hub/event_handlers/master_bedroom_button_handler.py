9435431f5528ec3bf59ea9ffc3f57ca91cd7e5ad
Jim Stys <jstys92@gmail.com>
2019-11-17 18:47:13 -0500

    Add handling for master bedroom closet button

--- appdaemon/apps/automation_hub/event_handlers/master_bedroom_button_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/master_bedroom_button_handler.py
@@ -4,13 +4,18 @@ from util import hassutil
 from util.entity_map import name_map
 from events.button_click_event import ButtonClickEvent
 from actions.media_player_action import MediaPlayerAction
+from actions.light_action import LightAction
 from automation_hub import state_machine
 
 def event_filter(event):
     return event.button_name == "master_bedroom_button"
 
+def closet_filter(event):
+    return event.button_name == "master_bedroom_closet_button"
+
 def register_callbacks():
     event_dispatcher.register_callback(on_button_clicked, ButtonClickEvent.__name__, event_filter=event_filter)
+    event_dispatcher.register_callback(on_closet_button_clicked, ButtonClickEvent.__name__, event_filter=closet_filter)
     
 def on_button_clicked(event):
     if event.click_type == "single":
@@ -19,6 +24,15 @@ def on_button_clicked(event):
         on_double_click(event)
     elif event.click_type == "long_click_press":
         on_long_press(event)
+
+def on_closet_button_clicked(event):
+    if event.click_type == "single":
+        on_closet_single_click(event)
+
+def on_closet_single_click(event):
+    logger.info("Master Bedroom Closet Button single clicked")
+
+    LightAction().add_light("master_bedroom_closet_lights").toggle()
 
 def on_single_click(event):
     logger.info("Master Bedroom Button single clicked")
