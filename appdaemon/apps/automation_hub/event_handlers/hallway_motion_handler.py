5af10503fe0e264f0deda16e86518ee67af5fd13
Jim Stys <jstys92@gmail.com>
2019-11-19 17:47:00 +0000

    Update motion handlers for xiaomi devices to rely completely on the motion triggered events with per-area timeouts

--- appdaemon/apps/automation_hub/event_handlers/hallway_motion_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/hallway_motion_handler.py
@@ -5,7 +5,6 @@ from automation_hub import state_machine
 from automation_hub import timer_manager
 from util import logger
 from events.xiaomi_motion_triggered_event import XiaomiMotionTriggeredEvent
-from events.motion_cleared_event import MotionClearedEvent
 from actions.light_action import LightAction
 
 def event_filter(event):
@@ -13,22 +12,17 @@ def event_filter(event):
 
 def register_callbacks():
     event_dispatcher.register_callback(on_motion_triggered, XiaomiMotionTriggeredEvent.__name__, event_filter=event_filter)
-    event_dispatcher.register_callback(on_motion_cleared, MotionClearedEvent.__name__, event_filter=event_filter)
     
 def on_motion_triggered(event):
     logger.info("Hallway motion detected")
     
     timer_manager.cancel_timer("hallway_motion_timer")
-    timer_manager.start_timer("hallway_motion_timer", functools.partial(on_motion_cleared, "hallway_motion_timer"), minutes=20)
+    timer_manager.start_timer("hallway_motion_timer", lights_off, minutes=5)
     
     if not state_machine.get_state(state_machine.SLEEP_STATE):
         LightAction().add_light("hallway_lights").turn_on()
     else:
-        LightAction().add_lights(["gateway_light", "first_floor_staircase_led"]).turn_on()
+        LightAction().add_lights(["first_floor_staircase_led"]).turn_on()
 
-def on_motion_cleared(event):
-    logger.info("Hallway motion cleared")
-    
-    if event != "hallway_motion_timer":
-        timer_manager.cancel_timer("hallway_motion_timer")
-    LightAction().add_lights(["hallway_lights", "gateway_light", "first_floor_staircase_led"]).turn_off()
+def lights_off():
+    LightAction().add_lights(["hallway_lights", "first_floor_staircase_led"]).turn_off()
--- appdaemon/apps/automation_hub/event_handlers/landing_motion_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/landing_motion_handler.py
@@ -5,7 +5,6 @@ from automation_hub import state_machine
 from automation_hub import timer_manager
 from util import logger
 from events.xiaomi_motion_triggered_event import XiaomiMotionTriggeredEvent
-from events.motion_cleared_event import MotionClearedEvent
 from actions.light_action import LightAction
 
 def event_filter(event):
@@ -13,19 +12,14 @@ def event_filter(event):
 
 def register_callbacks():
     event_dispatcher.register_callback(on_motion_triggered, XiaomiMotionTriggeredEvent.__name__, event_filter=event_filter)
-    event_dispatcher.register_callback(on_motion_cleared, MotionClearedEvent.__name__, event_filter=event_filter)
     
 def on_motion_triggered(event):
     logger.info("Landing motion detected")
     
     timer_manager.cancel_timer("landing_motion_timer")
-    timer_manager.start_timer("landing_motion_timer", functools.partial(on_motion_cleared, "landing_motion_timer"), minutes=20)
+    timer_manager.start_timer("landing_motion_timer", lights_off, minutes=5)
     
     LightAction().add_light("landing_light").turn_on()
 
-def on_motion_cleared(event):
-    logger.info("Landing motion cleared")
-    
-    if event != "landing_motion_timer":
-        timer_manager.cancel_timer("landing_motion_timer")
+def lights_off():
     LightAction().add_light("landing_light").turn_off()
