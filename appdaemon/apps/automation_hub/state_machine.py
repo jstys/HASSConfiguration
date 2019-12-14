7bbe5f5aa4e81a66a6094bb04eb41b4f77f9421f
Jim Stys <jstys92@gmail.com>
2019-12-14 09:22:59 -0500

    Fix missing namespace in state_machine calls.

--- appdaemon/apps/automation_hub/state_machine.py
+++ appdaemon/apps/automation_hub/state_machine.py
@@ -7,25 +7,25 @@ def set_api_handle(handle):
     API_HANDLE = handle
 
 def is_sleep_state_enabled():
-    return API_HANDLE.get_state(name_map["sleep_mode"]) == "on"
+    return API_HANDLE.get_state(name_map["sleep_mode"], namespace="hass") == "on"
 
 def is_guest_state_enabled():
-    return API_HANDLE.get_state(name_map["guest_mode"]) == "on"
+    return API_HANDLE.get_state(name_map["guest_mode"], namespace="hass") == "on"
 
 def is_sun_up():
-    return API_HANDLE.get_state(name_map["sun"]) == "above_horizon"
+    return API_HANDLE.get_state(name_map["sun"], namespace="hass") == "above_horizon"
 
 def is_jim_home():
-    return API_HANDLE.get_state(name_map["jim_presence"]) == "home"
+    return API_HANDLE.get_state(name_map["jim_presence"], namespace="hass") == "home"
 
 def is_erica_home():
-    return API_HANDLE.get_state(name_map["erica_presence"]) == "home"
+    return API_HANDLE.get_state(name_map["erica_presence"], namespace="hass") == "home"
 
 def is_nobody_home():
     return API_HANDLE.noone_home()
 
 def get_thermostat_mode():
-    return API_HANDLE.get_state(name_map["thermostat_mode"])
+    return API_HANDLE.get_state(name_map["thermostat_mode"], namespace="hass")
 
 def is_christmas_lights_enabled():
-    return API_HANDLE.get_state(name_map["christmas_lights_mode"]) == "on"
+    return API_HANDLE.get_state(name_map["christmas_lights_mode"], namespace="hass") == "on"
