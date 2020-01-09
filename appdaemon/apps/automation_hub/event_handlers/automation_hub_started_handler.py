0c9aa5389ff8a5f67105ce348a2055d5226477ef
Jim Stys <jstys92@gmail.com>
2020-01-08 22:48:45 -0500

    Add stub for handling start of automation hub

---
+++ appdaemon/apps/automation_hub/event_handlers/automation_hub_started_handler.py
@@ -0,0 +1,12 @@
+from automation_hub import event_dispatcher
+from automation_hub import state_machine
+from util import logutil
+from events.automation_hub_started_event import AutomationHubStartedEvent
+
+logger = logutil.get_logger("automation_hub")
+
+def register_callbacks():
+    event_dispatcher.register_callback(on_start, AutomationHubStartedEvent.__name__)
+    
+def on_start(event):
+    logger.info("Automation Hub Started")
