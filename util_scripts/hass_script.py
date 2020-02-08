cd70a0e5a7e2d920a11bf10e6fbf04d191d78bfd
Jim Stys <jstys92@gmail.com>
2020-02-08 10:37:24 -0500

    Cleanup dead code/scripts

--- util_scripts/hass_script.py
+++
@@ -1,7 +0,0 @@
-#!/srv/homeassistant/bin/python3
-import sys
-import homeassistant.remote as remote
-
-api = remote.API('127.0.0.1')
-
-remote.call_service(api, "script", sys.argv[1])
--- voice_assistant/tts_platforms/itts.py
+++
@@ -1,9 +0,0 @@
-class ITTS(object):
-    def __init__(self, config):
-        self._config = config
-    
-    def speak(self, message):
-        pass
-    
-    def validate_config(self):
-        pass
