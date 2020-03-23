d989761870aab9b8e83bdbdef73e27fd6ea5cfd7
Jim Stys <jstys92@gmail.com>
2020-03-23 19:12:37 -0400

    Update hass_regen to grab cover templates

--- hass_regen.py
+++ hass_regen.py
@@ -15,7 +15,8 @@ template_files = {
     "hass/template/media_player/universal.yaml": "hass/media_player/universal.yaml",
     "hass/template/light/lightgroups.yaml": "hass/light/lightgroups.yaml",
     "hass/template/logbook.yaml" : "hass/logbook.yaml",
-    "hass/template/binary_sensor/appliance.yaml": "hass/binary_sensor/appliance.yaml"
+    "hass/template/binary_sensor/appliance.yaml": "hass/binary_sensor/appliance.yaml",
+    "hass/template/cover/template.yaml": "hass/cover/template.yaml"
 }
 
 with open("entity_map.yaml", "r") as yamlfile:
