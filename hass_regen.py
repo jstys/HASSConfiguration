80cde50d0b9025643468da62e5289b880964eebc
Jim Stys <jstys92@gmail.com>
2020-08-10 17:35:43 -0400

    Remove ui-lovelace from templates.  Remove yeelights for master bedroom fixture and replace with zha group.

--- hass_regen.py
+++ hass_regen.py
@@ -9,7 +9,6 @@ def name_to_friendly_name(name):
 customized = {}
 entity_map = {}
 template_files = {
-    "hass/template/ui-lovelace.yaml": "hass/ui-lovelace.yaml",
     "hass/template/climate.yaml": "hass/climate.yaml",
     "hass/template/sensor/battery.yaml": "hass/sensor/battery.yaml",
     "hass/template/media_player/universal.yaml": "hass/media_player/universal.yaml",
