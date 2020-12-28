77638ee56c9ecab564a2de19a02f912c45f21d6d
Jim Stys <jstys92@gmail.com>
2020-12-28 17:33:18 -0500

    Add the attic_door_handler

---
+++ appdaemon/apps/automation_hub/event_handlers/attic_door_handler.py
@@ -0,0 +1,31 @@
+import functools
+
+from automation_hub import event_dispatcher
+from automation_hub import timer_manager
+from util import logutil
+from events.door_open_event import DoorOpenEvent
+from events.door_closed_event import DoorClosedEvent
+from actions.light_action import LightAction
+
+logger = logutil.get_logger("automation_hub")
+
+def event_filter(event):
+    return event.name == "attic_door_sensor"
+
+def register_callbacks():
+    event_dispatcher.register_callback(on_door_opened, DoorOpenEvent.__name__, event_filter=event_filter)
+    event_dispatcher.register_callback(on_door_closed, DoorClosedEvent.__name__, event_filter=event_filter)
+    
+def on_door_opened(event):
+    logger.info("Attic door opened")
+
+    timer_manager.cancel_timer("attic_door_timer")
+    
+    LightAction().add_light("attic_lights").turn_on()
+
+def on_door_closed(event):
+    logger.info("Attic door closed")
+    timer_manager.start_timer("attic_door_timer", lights_off, minutes=20)
+
+def lights_off():
+    LightAction().add_lights(["attic_lights"]).turn_off()
