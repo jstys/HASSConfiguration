a0aebce777094474796ad7a8d7c5ce34081e5031
Jim Stys <jstys92@gmail.com>
2021-03-19 14:27:38 -0400

    Fix missing register_callback in unavailable_event_handler

--- appdaemon/apps/automation_hub/event_handlers/unavailable_event_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/unavailable_event_handler.py
@@ -4,12 +4,14 @@ import event_dispatcher
 import timer_manager
 from util import logutil
 from events.unavailable_event import UnavailableEvent
+from events.available_event import AvailableEvent
 from actions.push_notify_action import PushNotifyAction
 
 logger = logutil.get_logger("automation_hub")
 
 def register_callbacks():
     event_dispatcher.register_callback(on_unavailable, UnavailableEvent.__name__)
+    event_dispatcher.register_callback(on_available, AvailableEvent.__name__)
 
 def on_available(event):
     logger.info(f"Received available for {event.name}")
