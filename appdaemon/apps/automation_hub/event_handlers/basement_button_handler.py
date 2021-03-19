e4dd6ec9b0f7784ae5dedffe49d8eb67c6f24c11
Jim Stys <jstys92@gmail.com>
2021-03-19 14:43:58 -0400

    Add support for toggling fan on long click

--- appdaemon/apps/automation_hub/event_handlers/basement_button_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/basement_button_handler.py
@@ -3,6 +3,7 @@ from util import logutil
 from util import hassutil
 from util.entity_map import name_map
 from events.button_click_event import ButtonClickEvent
+from actions.light_action import LightAction
 
 logger = logutil.get_logger("automation_hub")
 
@@ -16,8 +17,15 @@ def register_callbacks():
 def on_button_clicked(event):
     if event.click_type == "single":
         on_single_click(event)
+    elif event.click_type == "long_click_press":
+        on_long_press(event)
 
 def on_single_click(event):
     logger.info("Basement Button single clicked")
 
     hassutil.toggle(hassutil.Entity(name_map["workout_mode"]))
+
+def on_long_press(event):
+    logger.info("Basement Button long press")
+
+    LightAction().add_light("basement_fan").toggle()
