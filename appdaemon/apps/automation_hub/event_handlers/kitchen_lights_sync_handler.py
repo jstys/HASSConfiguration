b0f167d196501f3ce244d69c67f8c430b23e16ae
Jim Stys <jstys92@gmail.com>
2021-06-24 18:32:07 -0400

    Update kitchen_lights_sync_handler to use light events

--- appdaemon/apps/automation_hub/event_handlers/kitchen_lights_sync_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/kitchen_lights_sync_handler.py
@@ -1,7 +1,7 @@
 import event_dispatcher
 from util import logutil
-from events.switch_on_event import SwitchOnEvent
-from events.switch_off_event import SwitchOffEvent
+from events.light_on_event import LightOnEvent
+from events.light_off_event import LightOffEvent
 from actions.light_action import LightAction
 
 logger = logutil.get_logger("automation_hub")
@@ -10,8 +10,8 @@ def event_filter(event):
     return event.name == "kitchen_fixture"
 
 def register_callbacks():
-    event_dispatcher.register_callback(handle_turned_on, SwitchOnEvent.__name__, event_filter=event_filter)
-    event_dispatcher.register_callback(handle_turned_off, SwitchOffEvent.__name__, event_filter=event_filter)
+    event_dispatcher.register_callback(handle_turned_on, LightOnEvent.__name__, event_filter=event_filter)
+    event_dispatcher.register_callback(handle_turned_off, LightOffEvent.__name__, event_filter=event_filter)
 
 def handle_turned_on(event):
     LightAction().add_light("kitchen_hi_hats").turn_on()
