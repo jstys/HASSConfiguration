3f95e08eed5f0c8897a6e9b5584ad4531243f62f
Jim Stys <jstys92@gmail.com>
2022-03-17 22:10:57 -0400

    Fix auto_end_sleep_mode to run the disabling as a oneoff task

--- appdaemon/apps/automation_hub/scheduled_tasks/daily/auto_end_sleep_mode.py
+++ appdaemon/apps/automation_hub/scheduled_tasks/daily/auto_end_sleep_mode.py
@@ -1,11 +1,21 @@
 import state_machine
+import timer_manager
+from util import hassutil
 
-def get_starttime(is_weekend: bool):
-    if is_weekend:
+START_TIME = "08:00:00"
+
+def is_weekend():
+    return hassutil.get_current_date().weekday() > 4
+
+def get_starttime(weekend: bool):
+    if weekend:
         return "10:00:00"
     else:
         return "09:00:00"
 
-def callback():
+def _disable_sleep_state():
     if state_machine.is_enabled("sleep_mode"):
         state_machine.disable_sleep_state()
+
+def callback():
+    timer_manager.schedule_oneoff_task("disable_sleep_state", _disable_sleep_state, get_starttime(is_weekend()))
--- appdaemon/apps/automation_hub/scheduler.py
+++ appdaemon/apps/automation_hub/scheduler.py
@@ -1,10 +1,6 @@
 import timer_manager
-from util import hassutil
 from scheduled_tasks.daily import office_heatup
 from scheduled_tasks.daily import auto_end_sleep_mode
-
-def is_weekend():
-    return hassutil.get_current_date().weekday() > 4
 
 def schedule_daily_tasks():
     timer_manager.schedule_daily_task("office_heatup", office_heatup.callback, office_heatup.START_TIME)
