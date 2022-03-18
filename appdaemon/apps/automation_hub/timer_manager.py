f9aa412e8498dc24c5eb2a18be18257844e522df
Jim Stys <jstys92@gmail.com>
2022-03-17 21:57:48 -0400

    Add schedule_oneoff_task to timer_manager

--- appdaemon/apps/automation_hub/timer_manager.py
+++ appdaemon/apps/automation_hub/timer_manager.py
@@ -13,6 +13,11 @@ def set_api_handle(handle):
     global API_HANDLE
     API_HANDLE = handle
 
+def schedule_oneoff_task(name, callback, start):
+    if API_HANDLE:
+        logger.info(f"Scheduling oneoff callback: {name}")
+        API_HANDLE.run_once(callback, start)
+
 def schedule_polling_task(name, callback, interval):
     if API_HANDLE:
         logger.info(f"Scheduling polling callback: {name}")
