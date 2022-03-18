f16bed0fc4e90cf8c162b3bbecd345fff4f214b8
Jim Stys <jstys92@gmail.com>
2022-03-17 22:00:32 -0400

    Update scheduler to remove blue mountain polling and run office_heatup and auto_end_sleep_mode

--- appdaemon/apps/automation_hub/scheduler.py
+++ appdaemon/apps/automation_hub/scheduler.py
@@ -1,10 +1,14 @@
 import timer_manager
-from scheduled_tasks.daily import jim_wfh_check
-from scheduled_tasks.polling import blue_mtn_poll
+from util import hassutil
+from scheduled_tasks.daily import office_heatup
+from scheduled_tasks.daily import auto_end_sleep_mode
+
+def is_weekend():
+    return hassutil.get_current_date().weekday() > 4
 
 def schedule_daily_tasks():
-    timer_manager.schedule_daily_task("jim_wfh_check", jim_wfh_check.callback, jim_wfh_check.START_TIME)
+    timer_manager.schedule_daily_task("office_heatup", office_heatup.callback, office_heatup.START_TIME)
+    timer_manager.schedule_daily_task("auto_end_sleep_mode", auto_end_sleep_mode.callback, auto_end_sleep_mode.get_starttime(is_weekend()))
 
 def schedule_polling_tasks():
-    if blue_mtn_poll.ENABLED:
-        timer_manager.schedule_polling_task("blue_mtn_check", blue_mtn_poll.callback, blue_mtn_poll.INTERVAL)
+    pass
