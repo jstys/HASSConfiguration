b073786861e5394842dddac0efcf111b6e59731c
Jim Stys <jstys92@gmail.com>
2022-03-17 22:00:57 -0400

    Rename jim_wfh_check to office_heatup

---
+++ appdaemon/apps/automation_hub/scheduled_tasks/daily/office_heatup.py
@@ -0,0 +1,19 @@
+import state_machine
+import timer_manager
+from actions.thermostat_action import ThermostatAction
+from actions.light_action import LightAction
+
+START_TIME = "06:00:00"
+
+def _enable_office_heat():
+    heat_action = ThermostatAction().add_thermostat("office_heat")
+    heat_action.turn_on()
+    heat_action.set_temperature("74", "heat")
+
+def callback():
+    if state_machine.is_enabled("jim_wfh_calendar"):
+        LightAction().add_light("office_lights").turn_on()
+        if state_machine.is_heating_enabled():
+            _enable_office_heat()
+    else:
+        timer_manager.schedule_oneoff_task("office_heatup", _enable_office_heat, "08:00:00")
