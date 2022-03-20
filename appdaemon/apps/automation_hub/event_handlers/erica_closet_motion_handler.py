90541c76775bcd75b3dd8e791598251ac8d94f97
Jim Stys <jstys92@gmail.com>
2022-03-20 13:47:58 -0400

    Add handlers for closet door and lights

---
+++ appdaemon/apps/automation_hub/event_handlers/erica_closet_motion_handler.py
@@ -0,0 +1,32 @@
+import event_dispatcher
+import timer_manager
+from util import logutil
+from events.motion_triggered_event import MotionTriggeredEvent
+from events.motion_cleared_event import MotionClearedEvent
+from actions.light_action import LightAction
+import state_machine
+
+logger = logutil.get_logger("automation_hub")
+
+def motion_event_filter(event):
+    return event.name == "erica_closet_motion"
+
+def register_callbacks():
+    event_dispatcher.register_callback(on_motion_triggered, MotionTriggeredEvent.__name__, event_filter=motion_event_filter)
+    event_dispatcher.register_callback(on_motion_cleared, MotionClearedEvent.__name__, event_filter=motion_event_filter)
+    
+def on_motion_triggered(event):
+    logger.info("Erica closet motion detected")
+    
+    timer_manager.cancel_timer("erica_closet_motion_timer")
+    if state_machine.is_enabled("motion_lighting"):
+        LightAction().add_light("erica_closet_light").turn_on()
+
+def on_motion_cleared(event):
+    logger.info("Erica closet motion cleared")
+    timer_manager.start_timer("erica_closet_motion_timer", lights_off, minutes=20)
+    
+def lights_off():
+    if state_machine.is_enabled("motion_lighting"):
+        LightAction().add_light("erica_closet_light").turn_off()
+    
---
+++ appdaemon/apps/automation_hub/event_handlers/jim_closet_door_handler.py
@@ -0,0 +1,29 @@
+import event_dispatcher
+import timer_manager
+from util import logutil
+from events.door_open_event import DoorOpenEvent
+from events.door_closed_event import DoorClosedEvent
+from actions.light_action import LightAction
+
+logger = logutil.get_logger("automation_hub")
+
+def event_filter(event):
+    return event.name == "jim_closet_door_sensor"
+
+def register_callbacks():
+    event_dispatcher.register_callback(on_door_opened, DoorOpenEvent.__name__, event_filter=event_filter)
+    event_dispatcher.register_callback(on_door_closed, DoorClosedEvent.__name__, event_filter=event_filter)
+    
+def on_door_opened(event):
+    logger.info("Jim Closet door opened")
+
+    timer_manager.cancel_timer("jim_closet_door_timer")
+    
+    LightAction().add_light("jim_closet_light").turn_on()
+
+def on_door_closed(event):
+    logger.info("Jim Closet door closed")
+    timer_manager.start_timer("jim_closet_door_timer", lights_off, minutes=10)
+
+def lights_off():
+    LightAction().add_lights(["jim_closet_light"]).turn_off()
