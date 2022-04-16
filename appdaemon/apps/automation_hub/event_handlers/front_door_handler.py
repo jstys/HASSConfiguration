d01e1b474294a15844258cccdad8ce5cde30e83e
Jim Stys <jstys92@gmail.com>
2022-04-16 09:43:32 -0400

    Cleanup front_door_handler logic

--- appdaemon/apps/automation_hub/event_handlers/front_door_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/front_door_handler.py
@@ -1,11 +1,8 @@
 import event_dispatcher
-import state_machine
 from util import logutil
 from events.door_closed_event import DoorClosedEvent
 from events.door_open_event import DoorOpenEvent
 from events.lock_event import LockEvent
-from events.door_lock_notification_locked_event import DoorLockNotificationLockedEvent
-from events.door_lock_notification_unlocked_event import DoorLockNotificationUnlockedEvent
 from actions.push_notify_action import PushNotifyAction
 
 logger = logutil.get_logger("automation_hub")
@@ -23,14 +20,6 @@ def register_callbacks():
     event_dispatcher.register_callback(on_door_closed, DoorClosedEvent.__name__, event_filter=sensor_filter)
     event_dispatcher.register_callback(on_door_opened, DoorOpenEvent.__name__, event_filter=sensor_filter)
     event_dispatcher.register_callback(on_door_lock_changed, LockEvent.__name__, event_filter=lock_filter)
-    event_dispatcher.register_callback(on_door_locked_notification, DoorLockNotificationLockedEvent.__name__, event_filter=notify_filter)
-    event_dispatcher.register_callback(on_door_unlocked_notification, DoorLockNotificationUnlockedEvent.__name__, event_filter=notify_filter)
-    
-def on_door_locked_notification(event):
-    state_machine.set_state("front_entrance_lock", "locked")
-
-def on_door_unlocked_notification(event):
-    state_machine.set_state("front_entrance_lock", "unlocked")
 
 def on_door_closed(event):
     logger.info("Front door closed")
@@ -38,7 +27,7 @@ def on_door_closed(event):
 def on_door_opened(event):
     logger.info("Front door opened")
 
-def on_door_lock_changed(event):
+def on_door_lock_changed(event: LockEvent):
     if event.is_locked:
         logger.info("Front door locked")
         PushNotifyAction().add_target("jim_cell").set_message("Front door locked", notification_id="front-door-lock").notify()
