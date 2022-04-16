cb022aa94d6949a8d895620fda8764b3a6c2bb41
Jim Stys <jstys92@gmail.com>
2022-04-16 09:43:46 -0400

    Add garage notifications and turn on lights when door is opened

--- appdaemon/apps/automation_hub/event_handlers/garage_door_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/garage_door_handler.py
@@ -1,9 +1,9 @@
 import event_dispatcher
-import state_machine
 from util import logutil
 from events.door_closed_event import DoorClosedEvent
 from events.door_open_event import DoorOpenEvent
 from actions.light_action import LightAction
+from actions.push_notify_action import PushNotifyAction
 
 logger = logutil.get_logger("automation_hub")
 
@@ -16,7 +16,10 @@ def register_callbacks():
     
 def on_door_closed(event):
     logger.info("Garage door closed")
+    PushNotifyAction().add_target("jim_cell").set_message("Garage door closed", notification_id="garage-door-state").notify()
 
 def on_door_opened(event):
     logger.info("Garage door opened")
+    PushNotifyAction().add_target("jim_cell").set_message("Garage door closed", notification_id="garage-door-state").notify()
+    LightAction().add_light("garage_lights").turn_on()
     
