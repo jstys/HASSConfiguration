6155ab1b190a1f1f0f483287a9fdd420db683051
Jim Stys <jstys92@gmail.com>
2022-11-29 12:32:30 -0500

    Change notificatoin to sensor for owntracks connnection

--- appdaemon/apps/automation_hub/event_handlers/owntracks_event_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/owntracks_event_handler.py
@@ -1,6 +1,7 @@
 import json
 
 import event_dispatcher
+import api_handle
 from util import logger
 from events.mqtt_event import MQTTEvent
 from actions.push_notify_action import PushNotifyAction
@@ -18,9 +19,9 @@ def on_owntracks_event(event: MQTTEvent):
     owntracks_json = json.loads(event.payload)
     payload_type = owntracks_json['_type']
     if payload_type == "lwt":
-        PushNotifyAction().add_targets("jim_cell").set_message(f"{owntracks_user} is offline", notification_id=f"owntracks-{owntracks_user}-status").notify()
+        api_handle.instance.set_state(f"binary_sensor.{owntracks_user}_owntracks_connected", False)
         status_map[owntracks_user] = False
     elif payload_type == "encrypted":
         if owntracks_user in status_map and not status_map[owntracks_user]:
-            PushNotifyAction().add_targets("jim_cell").set_message(f"{owntracks_user} is online again", notification_id=f"owntracks-{owntracks_user}-status").notify()
+            api_handle.instance.set_state(f"binary_sensor.{owntracks_user}_owntracks_connected", True)
             status_map[owntracks_user] = True
