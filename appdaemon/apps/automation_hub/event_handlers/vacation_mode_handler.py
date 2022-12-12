096bf5ca20e5bdec774d565e67870df0dcbf68f3
jstys <jstys92@gmail.com>
2022-12-12 15:59:17 -0500

    Add vacation_mode_handler

---
+++ appdaemon/apps/automation_hub/event_handlers/vacation_mode_handler.py
@@ -0,0 +1,31 @@
+import event_dispatcher
+import state_machine
+from util import logger
+from events.input_event import InputEvent
+from actions.light_action import LightAction
+from actions.thermostat_action import ThermostatAction
+
+def event_filter(event):
+    return event.name == "vacation_mode"
+
+def register_callbacks():
+    event_dispatcher.register_callback(on_state_changed, InputEvent.__name__, event_filter=event_filter)
+    
+def on_state_changed(event):
+    if event.new == "off":
+        on_disabled(event)
+    elif event.new == "on":
+        on_enabled(event)
+    else:
+        logger.warning("Invalid state transition for vacation mode")
+
+def on_enabled(event):
+    if state_machine.is_heating_enabled():
+        ThermostatAction().add_thermostat("oil_thermostat").set_temperature(state_machine.get_number("vacation_heat"), "heat")
+
+    if state_machine.is_enabled("christmas_lights_mode"):
+        LightAction().add_light("christmas_tree_lights").turn_off()
+
+def on_disabled(event):
+    if state_machine.is_heating_enabled():
+        ThermostatAction().add_thermostat("oil_thermostat").set_temperature(state_machine.get_number("normal_heat"), "heat")
