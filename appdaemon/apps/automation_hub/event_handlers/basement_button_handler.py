f48dcb6a5fba816642e97534721ac0ce11c89b14
Jim Stys <jstys92@gmail.com>
2024-05-18 14:50:58 -0400

    Update basment button handler

--- appdaemon/apps/automation_hub/event_handlers/basement_button_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/basement_button_handler.py
@@ -2,20 +2,20 @@ import event_dispatcher
 from util import logger
 from util import hassutil
 from util.entity_map import name_map
-from events.button_click_event import ButtonClickEvent
+from events.mqtt_event import MQTTEvent
 from actions.switch_action import SwitchAction
 
 def event_filter(event):
-    return event.button_name == "Basement Switch Button"
+    return event.topic.startswith("zigbee2mqtt") and event.topic.endswith("action")
 
 
 def register_callbacks():
-    event_dispatcher.register_callback(on_button_clicked, ButtonClickEvent.__name__, event_filter=event_filter)
+    event_dispatcher.register_callback(on_button_clicked, MQTTEvent.__name__, event_filter=event_filter)
     
-def on_button_clicked(event):
-    if event.click_type == "single":
+def on_button_clicked(event: MQTTEvent):
+    if event.payload == "single":
         on_single_click(event)
-    elif event.click_type == "long_click_press":
+    elif event.payload == "long":
         on_long_press(event)
 
 def on_long_press(event):
