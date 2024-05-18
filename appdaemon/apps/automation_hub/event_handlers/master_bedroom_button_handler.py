5b958aa687f5dc72cd259d2326e72c8e6957c678
Jim Stys <jstys92@gmail.com>
2024-05-18 14:43:55 -0400

    Update appdaemon to listen for zigbee2mqtt click events

--- appdaemon/apps/automation_hub/event_handlers/master_bedroom_button_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/master_bedroom_button_handler.py
@@ -2,32 +2,30 @@ import event_dispatcher
 from util import logger
 from util import hassutil
 from util.entity_map import name_map
+from events.mqtt_event import MQTTEvent
 from events.button_click_event import ButtonClickEvent
 from actions.media_player_action import MediaPlayerAction
 from actions.light_action import LightAction
 import state_machine
 
-def event_filter(event):
-    return event.button_name == "Master Bedroom Bed Switch Button"
-
-def closet_filter(event):
-    return event.button_name == "Master Bedroom Closet Switch Button"
+def event_filter(event: MQTTEvent):
+    return event.topic.startswith("zigbee2mqtt") and event.topic.endswith("action")
 
 def register_callbacks():
     event_dispatcher.register_callback(on_button_clicked, ButtonClickEvent.__name__, event_filter=event_filter)
-    event_dispatcher.register_callback(on_closet_button_clicked, ButtonClickEvent.__name__, event_filter=closet_filter)
     
-def on_button_clicked(event):
-    if event.click_type == "single":
-        on_single_click(event)
-    elif event.click_type == "double":
-        on_double_click(event)
-    elif event.click_type == "long_click_press":
-        on_long_press(event)
-
-def on_closet_button_clicked(event):
-    if event.click_type == "single":
-        on_closet_single_click(event)
+def on_button_clicked(event: MQTTEvent):
+    device = event.topic.split("/")[1]
+    if device == "master_bedroom_button":
+        if event.payload == "single":
+            on_single_click(event)
+        elif event.payload == "double":
+            on_double_click(event)
+        elif event.payload == "long":
+            on_long_press(event)
+    elif device == "master_bedroom_closet_button":
+        if event.payload == "single":
+            on_closet_single_click(event)
 
 def on_closet_single_click(event):
     logger.info("Master Bedroom Closet Button single clicked")
