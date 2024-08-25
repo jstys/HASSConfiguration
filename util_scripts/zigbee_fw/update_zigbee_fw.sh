72b49b1ad4a7723a515fd3efeaff71af0f4eb5f5
Jim Stys <jstys92@gmail.com>
2024-08-25 11:39:00 -0400

    Use local file for zigbee fw

---
+++ util_scripts/zigbee_fw/update_zigbee_fw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FIRMWARE_FILE="CC1352P2_CC2652P_launchpad_coordinator_20240710.hex"
+SERIAL_DEV="/dev/ttyUSB0"
+
+docker run --rm \
+    --device $SERIAL_DEV:$SERIAL_DEV \
+    -e FIRMWARE_FILE=/firmware.hex \
+    -v $FIRMWARE_FILE:/firmware.hex
+    ckware/ti-cc-tool -ewv -p $SERIAL_DEV --bootloader-sonoff-usb
