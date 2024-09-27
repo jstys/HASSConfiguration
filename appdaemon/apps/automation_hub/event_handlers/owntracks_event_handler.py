4f0200c9b5a49cfa8ccfb891d76b61c91868af53
Jim Stys <jstys92@gmail.com>
2024-09-27 10:13:19 -0400

    Update owntracks event handler

--- appdaemon/apps/automation_hub/event_handlers/owntracks_event_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/owntracks_event_handler.py
@@ -7,19 +7,21 @@ from events.mqtt_event import MQTTEvent
 status_map = {}
 
 def event_filter(event: MQTTEvent):
-    return event.topic.startswith("owntracks/owntracks")
+    return event.topic.startswith("owntracks/")
 
 def register_callbacks():
     event_dispatcher.register_callback(on_owntracks_event, MQTTEvent.__name__, event_filter=event_filter)
 
 def on_owntracks_event(event: MQTTEvent):
-    owntracks_user = event.topic.split("/")[-1]
-    owntracks_json = json.loads(event.payload)
-    payload_type = owntracks_json['_type']
-    if payload_type == "lwt":
-        hassutil.set_state(hassutil.Entity(f"binary_sensor.{owntracks_user}_owntracks_connected"), "off")
-        status_map[owntracks_user] = False
-    elif payload_type == "encrypted":
-        if owntracks_user in status_map and not status_map[owntracks_user]:
-            hassutil.set_state(hassutil.Entity(f"binary_sensor.{owntracks_user}_owntracks_connected"), "on")
-            status_map[owntracks_user] = True
+    owntracks_user = event.topic.split("/")
+    if len(owntracks_user) == 3:
+        owntracks_user = owntracks_user[2]
+        owntracks_json = json.loads(event.payload)
+        payload_type = owntracks_json['_type']
+        if payload_type == "lwt":
+            hassutil.set_state(hassutil.Entity(f"binary_sensor.{owntracks_user}_owntracks_connected"), "off")
+            status_map[owntracks_user] = False
+        elif payload_type == "encrypted":
+            if owntracks_user in status_map and not status_map[owntracks_user]:
+                hassutil.set_state(hassutil.Entity(f"binary_sensor.{owntracks_user}_owntracks_connected"), "on")
+                status_map[owntracks_user] = True
