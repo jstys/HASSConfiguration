5f37480466b7e563f1ed44b1cfb008e673419051
Jim Stys <jstys92@gmail.com>
2024-12-06 17:01:32 -0500

    Don't modify heat with workout mode

--- appdaemon/apps/automation_hub/event_handlers/workout_mode_handler.py
+++ appdaemon/apps/automation_hub/event_handlers/workout_mode_handler.py
@@ -1,13 +1,11 @@
 import event_dispatcher
 import timer_manager
-import state_machine
 from util import logger
 from util import hassutil
 from events.input_event import InputEvent
 from actions.light_action import LightAction
 from actions.switch_action import SwitchAction
 from actions.media_player_action import MediaPlayerAction
-from actions.thermostat_action import ThermostatAction
 
 def event_filter(event):
     return event.name == "Workout Mode"
@@ -26,17 +24,9 @@ def on_state_changed(event):
 def on_enabled(event):
     hassutil.activate_scene("Workout Mode")
 
-    if state_machine.is_heating_enabled():
-        ThermostatAction().add_thermostat("Oil Thermostat").turn_off()
-
 def on_disabled(event):
     MediaPlayerAction().add_media_players(["Basement TV", "Basement GTV"]).turn_off()
     SwitchAction().add_switches(["Basement Fan Switch", "Basement Treadmill Fan"]).turn_off()
 
     light_action = LightAction().add_light("Basement Lights").turn_off
     timer_manager.start_timer("basement_stairs_motion_timer", light_action, minutes=10)
-
-    if state_machine.is_heating_enabled():
-        heat_action = ThermostatAction().add_thermostat("Oil Thermostat")
-        heat_action.turn_on()
-        heat_action.set_temperature(state_machine.get_number("Normal Heat"), "heat")
