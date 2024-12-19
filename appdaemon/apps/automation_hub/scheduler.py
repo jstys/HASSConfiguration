74864279b9b9339b0fdf0261af2e6d1586c60fa8
jstys <jstys92@gmail.com>
2024-12-19 07:00:06 -0800

    Fix scheduling name clashes

--- appdaemon/apps/automation_hub/scheduler.py
+++ appdaemon/apps/automation_hub/scheduler.py
@@ -3,8 +3,8 @@ from scheduled_tasks.daily import office_heatup
 from scheduled_tasks.daily import auto_end_sleep_mode
 
 def schedule_daily_tasks():
-    timer_manager.schedule_daily_task("office_heatup", office_heatup.callback, office_heatup.START_TIME)
-    timer_manager.schedule_daily_task("auto_end_sleep_mode", auto_end_sleep_mode.callback, auto_end_sleep_mode.START_TIME)
+    timer_manager.schedule_daily_task("office_heatup_check", office_heatup.callback, office_heatup.START_TIME)
+    timer_manager.schedule_daily_task("auto_end_sleep_mode_check", auto_end_sleep_mode.callback, auto_end_sleep_mode.START_TIME)
 
 def schedule_polling_tasks():
     pass
